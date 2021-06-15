e9079cdb38cd1d90452c4bf773af4df63177a478
Trofim Kremen <trofim.kremen@telekom.com>
2021-06-16 00:00:22 +0300

    init map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { DistributionService } from './distribution.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DistributionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
